1ae8e899742830d103c33552c499d28be0279a70
Md Masum Rana <masummx3@gmail.com>
2022-04-17 22:17:00 +0600

    toastidy add

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import auth from "../../../firebase.init";
 import img from "../../../image/Login/login.jpg";
 import Loading from "../../RequireAuth/Loading/Loading";
 import SocialMedia from "../../SocialMedia/SocialMedia";
 import "./Login.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const navigate =useNavigate()
     let location = useLocation();
+    const emailRef=useRef()
     let from = location.state?.from?.pathname || "/";
 
     const [
@@ -19,15 +22,19 @@ const Login = () => {
       loading,
       error,
     ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail, sending, error1] = useSendPasswordResetEmail(
+      auth
+    );
+
     if(user) {
       navigate(from, { replace: true });
     }
-    if(loading) {
+    if(loading ||sending) {
       <Loading></Loading>
     }
     let errorMessage
-    if(error) {
-      errorMessage =<p style={{color:'red'}}>{error.message}</p>
+    if(error ||error1) {
+      errorMessage =<p style={{color:'red'}}>{error?.message}</p>
     }
 
     const handleEmailAndPassword=(e)=>{
@@ -38,7 +45,16 @@ const Login = () => {
       // console.log(email,password);
     }
 
-
+    const notify =async() =>{ 
+      const email = emailRef.current.value
+    if(email){
+      await sendPasswordResetEmail(email);
+      toast("Sent Mail");
+    }
+    else{
+      toast('Please inter your email address')
+    }
+    }
     const handleReg=()=>{
       navigate('/register')
     }
@@ -53,7 +69,7 @@ const Login = () => {
           <Form onSubmit={handleEmailAndPassword} className="form-container">
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label className='fw-bold'>Email address</Form.Label>
-              <Form.Control name='email' type="email" required />
+              <Form.Control ref={emailRef} name='email' type="email" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label className='fw-bold'>Password</Form.Label>
@@ -69,11 +85,13 @@ const Login = () => {
             </Button>
           </Form>
           <p className='text-center'>New to Genius car? <Link to='/register' onClick={handleReg} className='text-danger text-decoration-none pe-auto'>Please Register</Link></p>
+          <p className='text-center'>Forget Password? <button  onClick={notify} className=' btn btn-link text-danger text-decoration-none pe-auto'>Reset password</button></p>
           <SocialMedia></SocialMedia>
         </div>
+       
         
       </div>
-      
+      <ToastContainer/>
     </div>
   );
 };
